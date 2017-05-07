e4d74e6b8886950431d96eb16b689e24fc4b0d1e
RanW <ran.wakshlak@ironsrc.com>
2017-05-07 08:57:33 +0300

    dumb_init

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:boron
 
+RUN wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64
+RUN chmod +x /usr/local/bin/dumb-init
+
 # Create app directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
@@ -13,3 +16,4 @@ COPY . /usr/src/app
 
 EXPOSE 3000
 CMD ["npm", "start"]
+ENTRYPOINT ["/usr/local/bin/dumb-init", "--"]
--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 var express = require('express');
 var app = express();
-var redis = require('redis');
-redisClient = redis.createClient('6379', 'redis');
 
 // Routes
 app.get('/', function(req, res) {
   res.send('Hello World!');
-});
-
-app.get('/set', function(req, res) {
-  var key = req.query.key;
-  var value = req.query.value;
-  redisClient.set('hello_world:' + key, value);
-  res.send('Key saved:' + key);
-});
-
-app.get('/get', function(req, res) {
-  var key = req.query.key
-  redisClient.get('hello_world:' + key , function(error, result) {
-    if (result) {
-    	res.send('Value found:' + result);
-    } else {
-	res.send('Key not found');
-    }
-  });
 });
 
 
